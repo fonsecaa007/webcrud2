212165bf981afac76d29511c8f0e5c1cbf637f5c
fonsecaa007 <106260824+fonsecaa007@users.noreply.github.com>
2022-12-01 00:09:24 -0300

    Add files via upload

---
+++ webcrud/webcrud.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE webcrud;
+
+USE webcrud;
+
+CREATE TABLE customers (
+  id int(11) NOT NULL,
+  name varchar(80) NOT NULL,
+  cpf_cnpj varchar(14) NOT NULL,
+  birthdate date NOT NULL,
+  address varchar(100) NOT NULL,
+  hood varchar(50) NOT NULL,
+  zip_code varchar(10) DEFAULT NULL,
+  city varchar(60) NOT NULL,
+  state varchar(2) NOT NULL,
+  phone varchar(20) DEFAULT NULL,
+  mobile varchar(20) DEFAULT NULL,
+  ie varchar(12) DEFAULT NULL,
+  created datetime NOT NULL,
+  modified datetime NOT NULL,
+  email varchar(60) DEFAULT NULL
+) ;
+
+--
+-- Índices para tabela customers
+--
+ALTER TABLE customers
+  ADD PRIMARY KEY (id);
+  
+ALTER TABLE customers
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+  CREATE TABLE users (
+    id int (11) AUTO_INCREMENT,
+    username varchar(255),
+    password varchar(255),
+    PRIMARY KEY(id)
+  );
+  
+  insert into users VALUES (
+      0, 'fonseca', SHA2('senha123', 256));
+
